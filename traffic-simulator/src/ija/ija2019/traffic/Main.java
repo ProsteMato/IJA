20b5275dcf98799a13a7eaaf693ba52ba9992b43
MisoKov <48593880+MisoKov@users.noreply.github.com>
2020-05-11 12:49:03 +0200

    Main.java - temp yaml parsing added to start method

--- traffic-simulator/src/ija/ija2019/traffic/Main.java
+++ traffic-simulator/src/ija/ija2019/traffic/Main.java
@@ -1,15 +1,36 @@
 package ija.ija2019.traffic;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import ija.ija2019.traffic.maps.Data;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import ija.ija2019.traffic.maps.Stop;
+import ija.ija2019.traffic.maps.Street;
+
+import java.io.File;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // ------- demonstracia citacia yaml ---------
+        YAMLFactory yaml_factory = new YAMLFactory();
+        ObjectMapper obj_mapper = new ObjectMapper(yaml_factory);
+        Data data = obj_mapper.readValue(new File("data/map.yml"), Data.class);
+
+        for (Street s : data.getStreets()){
+            System.out.println(s);
+        }
+
+        for (Stop s : data.getStops()){
+            System.out.println(s);
+        }
+        // -----------------------------------------------------
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Traffic Simulator");
         primaryStage.setScene(new Scene(root, 300, 275));
