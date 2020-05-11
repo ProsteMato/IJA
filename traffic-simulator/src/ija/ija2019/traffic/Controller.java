cc17a717e7f4751b9040ce74439db76d5a1814c1
MisoKov <48593880+MisoKov@users.noreply.github.com>
2020-05-11 20:41:53 +0200

    Controller.java - drawing map

--- traffic-simulator/src/ija/ija2019/traffic/Controller.java
+++ traffic-simulator/src/ija/ija2019/traffic/Controller.java
@@ -1,4 +1,36 @@
 package ija.ija2019.traffic;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Shape;
+
+import java.util.List;
+
 public class Controller {
+    public AnchorPane getMap() {
+        return map;
+    }
+
+    @FXML
+    private AnchorPane map;
+
+    @FXML
+    private void changeZoom(ScrollEvent event){
+        event.consume();
+        double scroll = event.getDeltaY();
+        double zoomScale;
+        if (scroll > 0){
+            zoomScale = 1.1;
+        } else {
+            zoomScale = 0.9;
+        }
+        map.setScaleX(zoomScale * map.getScaleX());
+        map.setScaleY(zoomScale * map.getScaleY());
+    }
+
+    public void draw(List<Shape> shapes){
+        for (Shape s : shapes)
+        map.getChildren().addAll(s);
+    }
 }
