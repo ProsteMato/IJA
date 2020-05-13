65db46dcac6802974a602ab65822bfc6a75e52e7
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-05-13 03:34:32 +0200

    created function for getting path and implemented function for getting next position of connection.

--- traffic-simulator/src/ija/ija2019/traffic/maps/Line.java
+++ traffic-simulator/src/ija/ija2019/traffic/maps/Line.java
@@ -1,16 +1,20 @@
 package ija.ija2019.traffic.maps;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Line implements ILine {
     private String id;
-    private List<Stop> stops;
+    private Map<String, List<Stop>> path;
     private List<Street> streets;
 
     public Line(String id) {
         this.id = id;
-        stops = new ArrayList<>();
         streets = new ArrayList<>();
     }
 
@@ -20,41 +24,53 @@ public class Line implements ILine {
         return id;
     }
 
-    public List<Stop> getStops() {
-        return stops;
-    }
-
     public List<Street> getStreets() {
         return streets;
     }
 
-    public List<Coordinate> getPath() {
-        List<Coordinate> path = new ArrayList<>();
-        Street lastStreet = null;
-        for (int i = 0; i < streets.size(); i++) {
-            if (lastStreet != null && !lastStreet.equals(streets.get(i))) {
-                path.add(lastStreet.end());
-                lastStreet = streets.get(i);
-            }
-            if (lastStreet == null) {
-                path.add(streets.get(i).begin());
-                lastStreet = streets.get(i);
-            }
-            if (i == streets.size() - 1) {
-                path.add(streets.get(i).end());
-            }
-            if (stops.get(i) != null) {
-                path.add(stops.get(i).getCoordinate());
+    public Map<String, List<Stop>> getPath() {
+        return path;
+    }
+
+    private void getStopsCoordinates(List<Coordinate> coordinates, Street street) {
+        if (this.path.get(street.getId()) != null) {
+            for (Stop stop : this.path.get(street.getId())) {
+                coordinates.add(stop.getCoordinate());
             }
         }
+    }
+
+    private void getStreetPathCoordinate(List<Coordinate> coordinates, Street firstStreet, Street secondStreet ) {
+        if (firstStreet.getEnd().equals(secondStreet.getBegin()) ||
+            firstStreet.getBegin().equals(secondStreet.getBegin())){
+            coordinates.add(secondStreet.getBegin());
+        } else if (firstStreet.getBegin().equals(secondStreet.getEnd()) ||
+                firstStreet.getEnd().equals(secondStreet.getEnd())) {
+            coordinates.add(secondStreet.getEnd());
+        }
+    }
+
+    public List<Coordinate> getCoordinates() {
+        List<Coordinate> path = new ArrayList<>();
+        for(int i = 0; i < streets.size(); i++) {
+            getStopsCoordinates(path, streets.get(i));
+            if (i != streets.size() - 1)
+                getStreetPathCoordinate(path, streets.get(i), streets.get(i + 1));
+        }
         return path;
+    }
+
+    public Coordinate calculateNewPosition(Coordinate source, Coordinate destination, double distance) {
+        Coordinate v = destination.subCoordinate(source);
+        Coordinate u = v.normalizeCoordinate();
+        return source.sumCoordinate(u.timesCoordinate(distance));
     }
 
     @Override
     public String toString() {
         return "Line{" +
                 "id='" + id + '\'' +
-                ", stops=" + stops +
+                ", path=" + path +
                 ", streets=" + streets +
                 '}';
     }
