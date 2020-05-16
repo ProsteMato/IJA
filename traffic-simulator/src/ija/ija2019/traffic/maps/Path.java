29e431df46272685ad8046ca0f8f62d735b303f2
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-05-16 02:55:36 +0200

    fixed connection and path for implementation of traffic

--- traffic-simulator/src/ija/ija2019/traffic/maps/Path.java
+++ traffic-simulator/src/ija/ija2019/traffic/maps/Path.java
@@ -9,13 +9,24 @@ import java.util.ListIterator;
 public class Path {
     private Stop source;
     private Stop destination;
-    private List<Coordinate> coordinates;
+    private boolean sameLine;
+    private List<Street> coordinates;
 
     private Path(){}
 
-    public Path(Stop source, List<Coordinate> coordinates) {
+    public Path(Stop source, Stop destination, boolean sameLine, List<Street> coordinates) {
         this.source = source;
         this.coordinates = coordinates;
+        this.sameLine = sameLine;
+        this.destination = destination;
+    }
+
+    public boolean isSameLine() {
+        return sameLine;
+    }
+
+    public void setSameLine(boolean sameLine) {
+        this.sameLine = sameLine;
     }
 
     public Stop getSource() {
@@ -34,19 +45,36 @@ public class Path {
         this.destination = destination;
     }
 
-    public List<Coordinate> getCoordinates() {
+    public List<Street> getCoordinates() {
         return coordinates;
     }
 
-    public void setCoordinates(List<Coordinate> coordinates) {
+    public void setCoordinates(List<Street> coordinates) {
         this.coordinates = coordinates;
+    }
+
+    private Coordinate commonCoordinate(Street firstStreet, Street secondStreet) {
+        if (firstStreet.getEnd().equals(secondStreet.getBegin()) ||
+                firstStreet.getBegin().equals(secondStreet.getBegin())){
+            return secondStreet.getBegin();
+        } else if (firstStreet.getBegin().equals(secondStreet.getEnd()) ||
+                firstStreet.getEnd().equals(secondStreet.getEnd())) {
+            return secondStreet.getEnd();
+        }
+        return null;
     }
 
     public List<Coordinate> getPath() {
         List<Coordinate> path = new ArrayList<>();
         path.add(source.getCoordinate());
-        if (coordinates != null) {
-            path.addAll(coordinates);
+        if (coordinates != null){
+            path.add(commonCoordinate(source.getStreet(), coordinates.get(0)));
+            for (int i = 0; i < coordinates.size() - 1; i++) {
+                path.add(commonCoordinate(coordinates.get(i), coordinates.get(i+1)));
+            }
+            path.add(commonCoordinate(coordinates.get(coordinates.size() - 1), destination.getStreet()));
+        } else if (!sameLine) {
+            path.add(commonCoordinate(source.getStreet(), destination.getStreet()));
         }
         path.add(destination.getCoordinate());
         return path;
@@ -58,18 +86,13 @@ public class Path {
 
     public double getPathLength() {
         double length = 0.0;
-        if (coordinates == null){
-            length += calculateLength(this.source.getCoordinate(), this.destination.getCoordinate());
-        } else {
-            length += calculateLength(this.source.getCoordinate(), this.coordinates.get(0));
-            for (int i = 0; i < coordinates.size() - 1; i++) {
-                length += calculateLength(this.coordinates.get(i), this.coordinates.get(i+1));
-            }
-            length += calculateLength(coordinates.get(coordinates.size() - 1), this.destination.getCoordinate());
+        List<Coordinate> coordinates = getPath();
+        for (int i = 0; i < coordinates.size() - 1; i++) {
+            length += calculateLength(coordinates.get(i), coordinates.get(i+1));
         }
         return length;
-
-
     }
+
+
 
 }
