dae3e210dc34fe5d52547f55c538433bcc9f82a7
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-05-17 22:32:07 +0200

    documentation for path

--- traffic-simulator/src/ija/ija2019/traffic/maps/ILine.java
+++
@@ -1,4 +0,0 @@
-package ija.ija2019.traffic.maps;
-
-public interface ILine {
-}
--- traffic-simulator/src/ija/ija2019/traffic/maps/Path.java
+++ traffic-simulator/src/ija/ija2019/traffic/maps/Path.java
@@ -1,11 +1,14 @@
 package ija.ija2019.traffic.maps;
 
-import sun.plugin.dom.core.CoreConstants;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
+/**
+ * This class represents path that is from source Stop to destination Stop.
+ * @version 1.0
+ * @author <a href="xkocim05@stud.fit.vutbr.cz">Martin Koči</a>
+ * @author <a href="xkoval17@stud.fit.vutbr.cz">Michal Koval</a>
+ */
 public class Path {
     private Stop source;
     private Stop destination;
@@ -21,34 +24,66 @@ public class Path {
         this.destination = destination;
     }
 
+    /**
+     * This method returns if path from source to destination is on one street.
+     * @return boolean
+     */
     public boolean isSameLine() {
         return sameLine;
     }
 
+    /**
+     * This method is setting if path from source to destination is on one street.
+     * @param sameLine boolean if the path is on one street.
+     */
     public void setSameLine(boolean sameLine) {
         this.sameLine = sameLine;
     }
 
+    /**
+     * This method return starting Stop of path.
+     * @return starting Stop of path.
+     */
     public Stop getSource() {
         return source;
     }
 
+    /**
+     * This method set starting Stop of path.
+     * @param source Stop.
+     */
     public void setSource(Stop source) {
         this.source = source;
     }
 
+    /**
+     * This method return ending Stop of path.
+     * @return ending Stop of path.
+     */
     public Stop getDestination() {
         return destination;
     }
 
+    /**
+     * This method sets ending Stop of path.
+     * @param destination Stop.
+     */
     public void setDestination(Stop destination) {
         this.destination = destination;
     }
 
+    /**
+     * This method returns Streets between source and end of the path.
+     * @return List of Streets.
+     */
     public List<Street> getCoordinates() {
         return coordinates;
     }
 
+    /**
+     * This method sets Streets that are between source and end of the path.
+     * @param coordinates List of Streets that are between source and end.
+     */
     public void setCoordinates(List<Street> coordinates) {
         this.coordinates = coordinates;
     }
@@ -64,6 +99,10 @@ public class Path {
         return null;
     }
 
+    /**
+     * This method return all streets that are in path.
+     * @return List of Streets that are in path.
+     */
     public List<Street> getStreets() {
         List<Street> streets = new ArrayList<>();
         streets.add(source.getStreet());
@@ -74,28 +113,11 @@ public class Path {
         return streets;
     }
 
-    public boolean isAffectedToTrafficChange(Street street, Coordinate currentDestination) {
-        for(Street s : getStreets()) {
-            if (s.equals(street) && isOnStreet(currentDestination)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isOnStreet(Coordinate position) {
-        List<Street> streets = getStreets();
-        if (destination.getCoordinate().equals(position)) {
-            return true;
-        }
-        for(Street street : streets) {
-            if (street.positionOnStreet(position)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    /**
+     * This method is for getting traffic from streets that position is on.
+     * @param position Current destination of out connection.
+     * @return traffic of street that position is on if not a good position is set it will return one.
+     */
     public double getTraffic(Coordinate position) {
         List<Street> streets = getStreets();
         if (destination.getCoordinate().equals(position)) {
@@ -106,9 +128,13 @@ public class Path {
                 return street.getTraffic();
             }
         }
-        return 0.6;
+        return 1.0;
     }
 
+    /**
+     * This method is creating path of coordinates from source to destination.
+     * @return list of coordinates from source to destination.
+     */
     public List<Coordinate> getPath() {
         List<Coordinate> path = new ArrayList<>();
         path.add(source.getCoordinate());
@@ -129,6 +155,10 @@ public class Path {
         return Math.sqrt(Math.pow(source.diffX(destination), 2) + Math.pow(source.diffY(destination), 2));
     }
 
+    /**
+     * This method calculates path length
+     * @return path length
+     */
     public double getPathLength() {
         double length = 0.0;
         List<Coordinate> coordinates = getPath();
