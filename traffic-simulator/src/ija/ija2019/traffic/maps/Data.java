b1935311f16ce485dc28c0ad8281b5990101a1b6
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-05-17 23:05:01 +0200

    Documentation data

--- traffic-simulator/src/ija/ija2019/traffic/maps/Data.java
+++ traffic-simulator/src/ija/ija2019/traffic/maps/Data.java
@@ -3,17 +3,30 @@ package ija.ija2019.traffic.maps;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is representing data for application.
+ * @version 1.0
+ * @author <a href="xkocim05@stud.fit.vutbr.cz">Martin Koči</a>
+ * @author <a href="xkoval17@stud.fit.vutbr.cz">Michal Koval</a>
+ */
 public class Data {
     private List<Street> streets;
     private List<Stop> stops;
     private List<Line> lines;
     private List<Connection> connections;
 
-    public Data(List<Street> streets, List<Stop> stops) {
+    public Data(List<Street> streets, List<Stop> stops, List<Line> lines, List<Connection> connections) {
         this.streets = streets;
         this.stops = stops;
+        this.lines = lines;
+        this.connections = connections;
     }
 
+    /**
+     * This function will return connection from list of connection by id
+     * @param id id of connection
+     * @return connection on success otherwise null
+     */
     public Connection getConnectionById(String id){
         for (Connection con : connections) {
             if (con.getId().equals(id)) {
@@ -23,6 +36,11 @@ public class Data {
         return null;
     }
 
+    /**
+     * This function will return street from list of streets by id
+     * @param id id of street
+     * @return street on success otherwise null
+     */
     public Street getStreetById(String id){
         for (Street s : streets){
             if (s.getId().equals(id)){
@@ -34,22 +52,42 @@ public class Data {
 
     private Data(){}
 
+    /**
+     * This method returns lines.
+     * @return lines.
+     */
     public List<Line> getLines() {
         return lines;
     }
 
+    /**
+     * This method returns connections.
+     * @return connections.
+     */
     public List<Connection> getConnections() {
         return connections;
     }
 
+    /**
+     * This method returns streets.
+     * @return streets.
+     */
     public List<Street> getStreets() {
         return streets;
     }
 
+    /**
+     * This method returns stops.
+     * @return stops.
+     */
     public List<Stop> getStops() {
         return stops;
     }
 
+    /**
+     * This method returns all drawable objects.
+     * @return all drawable objects.
+     */
     public List<Drawable> getDrawables() {
         List<Drawable> drawables = new ArrayList<>(streets);
         drawables.addAll(stops);
