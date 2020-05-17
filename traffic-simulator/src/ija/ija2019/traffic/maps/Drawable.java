03fbf5e8a42b682e07f97928cf7cba2582a72eb6
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-05-17 22:59:57 +0200

    documentation Drawable

--- traffic-simulator/src/ija/ija2019/traffic/maps/Drawable.java
+++ traffic-simulator/src/ija/ija2019/traffic/maps/Drawable.java
@@ -4,6 +4,16 @@ import javafx.scene.shape.Shape;
 
 import java.util.List;
 
+/**
+ * This interface is representing all objects that are Drawable on Scene
+ * @version 1.0
+ * @author <a href="xkocim05@stud.fit.vutbr.cz">Martin Koči</a>
+ * @author <a href="xkoval17@stud.fit.vutbr.cz">Michal Koval</a>
+ */
 public interface Drawable {
+    /**
+     * This method is returning all drawable objects that can be rendered
+     * @return drawable objects.
+     */
     List<Shape> getDrawableObjects();
 }
