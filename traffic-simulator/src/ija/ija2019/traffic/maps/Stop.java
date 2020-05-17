2944babd3f1d8fb580ad091ad3a87959eb393767
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-05-17 23:17:29 +0200

    Documenation Stop

--- traffic-simulator/src/ija/ija2019/traffic/maps/Stop.java
+++ traffic-simulator/src/ija/ija2019/traffic/maps/Stop.java
@@ -12,13 +12,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * This class is representation of stop.
+ * @version 1.0
+ * @author <a href="xkocim05@stud.fit.vutbr.cz">Martin Koči</a>
+ * @author <a href="xkoval17@stud.fit.vutbr.cz">Michal Koval</a>
+ */
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @JsonDeserialize(converter= Stop.StopDeserialize.class)
-public class Stop implements IStop, Drawable{
+public class Stop implements Drawable{
     private String id;
     private Coordinate coordinate;
     private Street street;
-    private List<Shape> drawableObjects = new ArrayList<>();
+    private List<Shape> drawableObjects;
 
     public Stop(String id, Coordinate coordinate, Street street) {
         this.id = id;
@@ -29,26 +35,49 @@ public class Stop implements IStop, Drawable{
 
     private Stop(){}
 
+    /**
+     * This function is setting drawable objects
+     */
     public void setDrawableObjects() {
+        drawableObjects = new ArrayList<>();
         drawableObjects.add(new Circle(coordinate.getX(), coordinate.getY(), 6, Color.RED));
     }
 
+    /**
+     * This method is adding stop to street.
+     */
     public void addStreetStop() {
         street.addStop(this);
     }
 
+    /**
+     * This method is returning coordinates of stop
+     * @return coordinates of stop
+     */
     public Coordinate getCoordinate() {
         return this.coordinate;
     }
 
+    /**
+     * This method is returning id of stop
+     * @return id of stop
+     */
     public String getId() {
         return this.id;
     }
 
+    /**
+     * This method is returning street of stop
+     * @return street of stop
+     */
     public Street getStreet() {
         return this.street;
     }
 
+    /**
+     * This method is setting street of stop
+     * @param s street on which is stop
+     */
     public void setStreet(Street s) {
         this.street = s;
     }
